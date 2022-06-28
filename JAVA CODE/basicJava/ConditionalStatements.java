735c16c27a90dbb9b5124fbb7ed449be179dbd83
vasanth2030 <vasanths2030@gmail.com>
2022-06-28 21:28:43 +0530

    day4

--- JAVA CODE/basicJava/ConditionalStatements.java
+++ JAVA CODE/basicJava/ConditionalStatements.java
@@ -5,8 +5,35 @@ import java.util.Scanner;
 public class ConditionalStatements {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter your age: ");
-		int age=sc.nextInt();
+		System.out.println("Enter a number: ");
+		int n=sc.nextInt();
+		
+		switch(n)
+		{
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Enter a number between 1-7");			
+		}
 		
 //		if(age>18 && age<30)
 //			System.out.println("Young adult");
@@ -18,8 +45,8 @@ public class ConditionalStatements {
 //			System.out.println("Child");
 //		}
 		
-		String result=age>18?"Adult":"Child";
-		
-		System.out.println(result);
+//		String result=age>18?"Adult":"Child";
+//		
+//		System.out.println(result);
 	}
 }
---
+++ JAVA CODE/basicJava/Loops.java
@@ -0,0 +1,18 @@
+package basicJava;
+
+import java.util.Scanner;
+
+public class Loops {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+		int i=1;
+		
+		while(i<=n)
+		{
+			System.out.println("Hello World "+i);
+			i++;
+		}
+	}
+}
