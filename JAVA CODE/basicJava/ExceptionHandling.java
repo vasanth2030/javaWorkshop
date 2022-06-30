c8b8b5a91ea745a703356c4382eb86bd6eab51ab
vasanth2030 <vasanths2030@gmail.com>
2022-06-30 21:27:27 +0530

    day6

---
+++ JAVA CODE/basicJava/ExceptionHandling.java
@@ -0,0 +1,34 @@
+package basicJava;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+	
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		try {
+			System.out.println("Enter two numbers: ");
+			int a= sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println("Division: "+ (a/b));
+//			throw new NumberFormatException("Manually thrown exception");
+		}catch(ArithmeticException e)
+		{
+			System.out.println("Inside ArithmeticException catch block");
+			System.out.println("Enter a non-zero divisor");
+		}catch(InputMismatchException e)
+		{
+			System.out.println("Inside InputMismatchException catch block");
+			System.out.println("Give a proper input");
+		}catch(Exception e)
+		{
+			System.out.println(e);
+			System.out.println("Inside exception block");
+		}finally {
+			System.out.println("Inside finally");
+		}
+		System.out.println("End of program.");
+	}
+
+}
