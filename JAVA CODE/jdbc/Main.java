e51c1770108d5fc7891515a86d622139b17bacf6
vasanth2030 <vasanths2030@gmail.com>
2022-07-14 21:10:04 +0530

    day15

--- JAVA CODE/jdbc/Main.java
+++ JAVA CODE/jdbc/Main.java
@@ -9,7 +9,28 @@ public class Main {
 		String url="jdbc:mysql://localhost:3306/mysql_workspace";
 		
 		Connection con = DriverManager.getConnection(url,uname,pass);
+		Statement st= con.createStatement();
 		
+		//read
+//		ResultSet rs= st.executeQuery("select * from student where student_id>2");
+//		while(rs.next())
+//		{
+//			System.out.print(rs.getInt(1)+" | "+rs.getString(2)+" | "+rs.getInt(3)+" | "
+//					+rs.getString(4));
+//			System.out.println();
+//		}
+		
+		//insert
+//		int n = st.executeUpdate("insert into student values (7, 'Rohan',21, 'rohan@gmail.com')");
+//		System.out.println(n+" rows affected");
+
+		//update
+//		int n = st.executeUpdate("update student set age=25 where student_id>3");
+//		System.out.println(n+" rows affected");
+
+		int n = st.executeUpdate("delete from student where student_id=7");
+		System.out.println(n+" rows affected");
+
 		System.out.println("Connection success.");
 	}
 }
